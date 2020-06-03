9b29e827b54c485660779f51fc07cb445bbc427c
byron158 <byronclaw@hotmail.com>
2020-06-03 15:36:37 -0700

    Fixed debugging prints for STMTX

--- esp32/STMCommTX.ino
+++ esp32/STMCommTX.ino
@@ -9,6 +9,7 @@
 bool send_header_stm(const uint32_t &data_size, const uint8_t &data_type){
   uint8_t header[HEADER_SIZE];
   constructHeader(header,data_size,data_type);
+  Serial.println("Sending header to STM");
   Serial2.write(header,HEADER_SIZE);
   Serial2.flush();
   return wait_for_ack_stm(50);
@@ -30,7 +31,7 @@ bool write_message_stm(const uint8_t* msg, const uint32_t &data_length){
     Serial.print(msg[i]);
     Serial.print(" ");
   }
-  Serial.println("\n");
+  Serial.println("");
   Serial2.flush();
   return wait_for_ack_stm(50);
 }
@@ -48,15 +49,27 @@ bool wait_for_ack_stm(int timeout){
     current_time = millis();
   }
   if(current_time-sec>timeout){
-    Serial.println("UART ACK timed out");
+    Serial.println("UART ACK timed out\n");
     return false;
   }
   if(Serial2.read() == (int) 'A'){
-    Serial.println("Recieved ACK UART");
+    Serial.println("Recieved ACK UART\n");
     return true;
   }
   else{
-    Serial.println("Wrong ACK recieved from UART");
+    Serial.println("Wrong ACK recieved from UART\n");
     return false;
   }
+}
+
+bool send_stop_command(){
+  byte msg = 0xFF; bool success = false;
+  if(!send_header_stm(1,CMD_STOP_SAMPLE)){
+    Serial.println("STOP CMD failed");
+    success = false;
+  }
+  if(!write_message_stm(&msg,1)){
+    success = false;
+  }
+  return success;
 }
