d26f75c905c00ce29f88179a8faa5bf8df7f286b
byron158 <byronclaw@hotmail.com>
2020-06-04 03:34:46 -0700

    Doesnt send wifi message if header fails

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -132,6 +132,7 @@ void IRAM_ATTR flagReadADCData(void) {
 }
 
 void upStreamTask(void* pvParameters) {
+  bool headerFailed = false;
   while (true) {
     if (interrupt_flag) {
       interrupt_flag = 0;
@@ -139,7 +140,12 @@ void upStreamTask(void* pvParameters) {
 
       if (verify_cmd_from_stm(incoming.data_type)) {
         if (isConnected && clientTX.connected()){
-          send_header_wifi(incoming.data_left, incoming.data_type);
+          if(send_header_wifi(incoming.data_left, incoming.data_type)){
+            headerFailed = false;
+          }
+          else{
+            headerFailed = true;
+          }
         }
         else{
           isConnected = false;
@@ -158,7 +164,7 @@ void upStreamTask(void* pvParameters) {
 //          Serial.println("\n");
           #endif
           read_message_stm(incoming.msg, readLength);
-          if (isConnected && clientTX.connected()){
+          if (isConnected && clientTX.connected()&&!headerFailed){
             write_message_wifi(incoming.msg, readLength);
           }
           else{
